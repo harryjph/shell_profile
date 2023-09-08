f0ae0dfaf0228dd8daf608db18aef1ee61fb0d85
Harry Phillips <harryjamesphillips@gmail.com>
2023-09-08 01:02:45 +0100

    clusterup

--- configs/scripts.sh
+++ configs/scripts.sh
@@ -4,3 +4,6 @@ export PATH="$HOME/.shell_profile/test_scripts:$PATH"
 # Shorthand for scripts
 alias myd="mysqld"
 alias chd="clickhoused"
+
+# clusterup magic
+alias clusterup="clusterup && export KUBECONFIG=~/.shell_profile/scripts/k3s/kubeconfig.yaml"
