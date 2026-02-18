9cf4a76f1f7f4ced677c8198991127804bc5c310
Harry Phillips <harryjamesphillips@gmail.com>
2026-02-18 20:29:43 +0000

    note

--- configs/zellij.sh
+++ configs/zellij.sh
@@ -3,6 +3,7 @@ alias z="zellij"
 alias zd="zellij attach --create default"
 
 # Initialise completions
+# TODO disabled because it's slow and produces a warning on ZSH startup :(
 #if command -v zellij 2>&1 >/dev/null; then
 #  if [ -n "$ZSH_VERSION" ]; then
 #      eval "$(zellij setup --generate-completion zsh)"
