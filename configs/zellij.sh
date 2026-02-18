1ef63407f587558008add962d3c9b60157c00da7
Harry Phillips <harryjamesphillips@gmail.com>
2026-02-18 20:29:13 +0000

    disable zellij completions

--- configs/zellij.sh
+++ configs/zellij.sh
@@ -3,10 +3,10 @@ alias z="zellij"
 alias zd="zellij attach --create default"
 
 # Initialise completions
-if command -v zellij 2>&1 >/dev/null; then
-  if [ -n "$ZSH_VERSION" ]; then
-      eval "$(zellij setup --generate-completion zsh)"
-  elif [ -n "$BASH_VERSION" ]; then
-      eval "$(zellij setup --generate-completion bash)"
-  fi
-fi
+#if command -v zellij 2>&1 >/dev/null; then
+#  if [ -n "$ZSH_VERSION" ]; then
+#      eval "$(zellij setup --generate-completion zsh)"
+#  elif [ -n "$BASH_VERSION" ]; then
+#      eval "$(zellij setup --generate-completion bash)"
+#  fi
+#fi
