d186b11d71ccb1bb6a45721112ad316777fdba83
Harry Phillips <harryjamesphillips@gmail.com>
2026-09-23 02:24:32 +0100

    more sandbox config

--- configs/sandbox.sh
+++ configs/sandbox.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# Basic sandbox config
 export SANDBOX_IMAGE="fedora"
 export SANDBOX_ENV="HOME:PATH:LD_LIBRARY_PATH:TERM"
-export SANDBOX_PATH="$PATH:$LD_LIBRARY_PATH:/home/linuxbrew"
+export SANDBOX_PATH="$PATH:$LD_LIBRARY_PATH"
+
+# Include Homebrew if it's installed
+if [ -d "/home/linuxbrew" ]; then
+    SANDBOX_PATH="$SANDBOX_PATH:/home/linuxbrew"
+fi
 
 # App-specific overrides
 export SANDBOX_PATH_RW_agy="~/.gemini"
+export SANDBOX_PATH_RW_claude="~/.claude"
+export SANDBOX_PATH_RW_codex="~/.codex"
 export SANDBOX_PATH_RW_opencode="~/.config/opencode:~/.local/share/opencode:~/.local/state/opencode:~/.cache/opencode"
+export SANDBOX_PATH_RW_pi="~/.pi"
